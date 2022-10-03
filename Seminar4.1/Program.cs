d62136d0fe72b63aae5737ae1b5233d5e9af5c8b
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-03 22:23:51 +0300

    выдает сумму чисел от 1 до а

---
+++ Seminar4.1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Принимает на вход число A и выдает сумму чисел от 1 до A.
+
+Console.WriteLine ("Введи число А: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма чисел от 1 до {A} равна {GetSum(A)}");
+
+int GetSum (int limit)
+{
+    int sum = 0;
+    for (int i = 1; i<=limit; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
