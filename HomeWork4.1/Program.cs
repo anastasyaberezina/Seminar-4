8c7c5f02d5879d10d580e5f80a7c80dc87fb5cf9
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-04 19:04:31 +0300

    программа, задающая  массив из 8 рандомных чисел

---
+++ HomeWork4.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
+
+int[] array = ArrayRandom(8);
+Console.WriteLine($"[{String.Join("; ", array)}]");
+
+int[] ArrayRandom (int length)
+{
+    int[] result = new int[length];
+    for(int i=0; i<length; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
