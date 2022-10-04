f6a3c1e244175234a2ca3afed76322383facd867
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-04 09:32:10 +0300

    выдает кол-во цифр во введенном числе

---
+++ Seminar4.2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Принимает число и выдает кол-во цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество цифр в этом числе {Quantity(num)}");
+
+int Quantity (int Array)
+{
+    int a = 2;
+    
+    for (int x = 1; x <= Array; x++)
+    {
+      a=a*1;
+    }
+    return a;
+}
