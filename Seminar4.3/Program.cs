b1c3978f5df795f1f053709a4eee1475c0f8892d
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-04 18:01:56 +0300

    произведение чисел от 1 до N

---
+++ Seminar4.3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Принимает число N и выдает произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Умножение цифр в промежутке от 1 до {N} равно {Multi(N)}");
+
+int Multi (int length)
+{ 
+  int x = 1; 
+
+  for (int i=1; i<=length; i++)
+  {
+    x= x*i;
+  }
+  return x;
+}
+
+  
