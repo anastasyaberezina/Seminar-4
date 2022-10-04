7d97ae1e3fc4708fd7eecc78bf56ab391281bae1
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-04 18:42:30 +0300

    массив случайных чисел из 0 и 1

---
+++ Seminar4.4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выводит массив из 8 элементов, заполненный нулями и единицами в случ порядке.
+
+int[] array = ArrayMethod(8);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int[] ArrayMethod (int size)
+{
+    int[] result = new int[size];
+    for(int i=0; i<size; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+    return result;
+}
